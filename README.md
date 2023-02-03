c1e96d84b527dc55f81bdeef308e0c1fe8df1f24
etherware-novice <candy@notarealaddr.com>
2023-02-02 23:12:54 -0600

    binds attack to a keybind, hp system

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ current progress:
 https://user-images.githubusercontent.com/73374039/216513379-bf9932d5-8749-473b-9e88-01d84db8711e.mp4
 
 TODO:
-- [ ] Dealing damage
-- [ ] Implementing game over
+- [x] Dealing damage
 - [ ] Damage numbers / HP bar
 - [ ] Turns system
 	- [ ] Implementing missing hurt/atk sprites
 	- [ ] Some form of turn control
 	- [ ] Enemy attack system
+    - [ ] Implementing game over
 - [ ] Choosing attacks
 - [ ] Multiple party members
 - [ ] Action commands
--- icecream.gd
+++ icecream.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
-
+var maxhp = 30
+var hp
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -8,7 +9,7 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hp = maxhp
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +17,10 @@ func _ready():
 #	pass
 
 
-func _on_beanie_attack():
+func _on_beanie_attack(dmg):
 	$AnimatedSprite.animation = "hurt"
 	yield($AnimatedSprite, "animation_finished")
+	hp = hp - dmg
+	if(hp < 0):
+		queue_free()
 	$AnimatedSprite.animation = "idle"
